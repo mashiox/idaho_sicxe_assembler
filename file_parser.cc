87db5a4968eeb672522a818c2193c8d4f3f21d5f
Phillip Domann <phillip.domann@gmail.com>
2016-02-19 11:27:48 -0800

    Added a method to read the entire file contents into memory and pass line by line into the tokenizer.

--- file_parser.cc
+++ file_parser.cc
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <cstdlib>
+#include <sstream>
 #include <stdexcept>
 #include <vector>
 #include "file_parser.h"
@@ -12,34 +13,28 @@ using namespace std;
  * Constrctor
  */
 file_parser::file_parser(string filename){
-    file = filename;
+    this->filename = filename;
+    source = NULL;
 }
 
 /**
  * Destructor
  */
 file_parser::~file_parser(){
+    delete source;
 }
 
 /**
  * Reads a file by line,
  */
 void file_parser::read_file(){
-    try {
-        infile.open(file.c_str(), ios::in);
-    } catch (ios_base::failure fail) {
-        throw file_parse_exception("error: failed to open file "+file);
+    source = get_file_contents();
+
+    istringstream sstream (*source);
+    string str;
+    while (getline(sstream, str)) {
+        // pass to tokenizer
     }
-    if (!infile) {
-        throw file_parse_exception("error: failed to open file "+file);
-    }
-    
-	string line;
-	while ( !infile.eof() ){
-		getline( infile, line );
-//		container.push_back(line);
-	}
-	infile.close();
 }
 
 /**
@@ -49,18 +44,18 @@ string file_parser::get_token(unsigned int row, unsigned int column){
     try {
         switch (column) {
             case 0:
-                return container.at(row).label;
+                return container.at(row).getlabel();
             case 1:
-                return container.at(row).opcode;
+                return container.at(row).getopcode();
             case 2:
-                return container.at(row).operand;
+                return container.at(row).getoperand();
             case 3:
-                return container.at(row).comment;
+                return container.at(row).getcomment();
             default:
-                throw file_parse_exception("error: get_token column is out of bounds");
+                throw file_parse_exception("Error: get token column is out of bounds, must be between 0 and 3");
         }
     } catch (out_of_range &oor) {
-        throw file_parse_exception("error: get_token row is out of bounds");
+        throw file_parse_exception("Error: get token row is out of bounds, must be between 0 and size()-1");
     }
 }
 
@@ -70,7 +65,7 @@ string file_parser::get_token(unsigned int row, unsigned int column){
 void file_parser::print_file(){
 	vector<line>::iterator iter;
 	for ( iter = container.begin() ; iter < container.end() ; iter++ ){
-        cout << iter->label << "\t" << iter->opcode << "\t" << iter->operand << "\t" << iter->comment << "\n";
+        // iomanip
 	}
 }
 
@@ -79,4 +74,38 @@ void file_parser::print_file(){
  */
 int file_parser::size(){
 	return (int)container.size();
+}
+
+/**
+ * Returns a pointer to a string with the file's contents
+ */
+
+const string* file_parser::get_file_contents() {
+    if (source != NULL) {
+        delete source;
+    }
+    
+    ifstream stream;
+    stream.open(filename.c_str(), ios::in);
+    
+    if (!stream.is_open()) {
+        throw file_parse_exception("Error: could not open file "+filename+". Check that the file exists or was properly entered.");
+    }
+    stream.seekg(0, ios::end);
+    long long filesize = stream.tellg();
+    stream.seekg(0, ios::beg);
+    
+    char* buffer = NULL;
+    buffer = new char[filesize+1];
+    if (buffer == NULL) {
+        stream.close();
+        throw file_parse_exception("Error: could not allocate memory to read source file.");
+    }
+    stream.read(buffer, filesize);
+    buffer[filesize] = '\0';
+    string* contents = new string(buffer);
+    
+    stream.close();
+    delete[] buffer;
+    return contents;
 }
