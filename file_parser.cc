c1e863aa3d3bb53d38e666733dcf924fddc2b9c8
Matt Walther <matt@mashio.net>
2016-02-22 23:40:11 -0800

    Last minute changed before due date.

--- file_parser.cc
+++ file_parser.cc
@@ -159,3 +159,23 @@ retoken:
     }
     return tokenstruct;
 }
+
+int main(int argc, const char *argv[]) {
+    if (argc != 2) {
+        cout << "Proper usage is " << argv[0] << " sourcefile\n";
+        return -1;
+    }
+    
+	file_parser* fp = new file_parser(argv[1]);
+    try {
+        fp->read_file();
+    } catch (file_parse_exception e) {
+        cout << e.getMessage() << "\n";
+        return 1;
+    }
+
+    fp->print_file();
+    delete fp;
+    
+    return 0;
+}
