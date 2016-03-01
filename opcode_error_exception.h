691e2dfd27ad9fe38d3f46dae89223da75fd159a
Phillip Domann <phillip.domann@gmail.com>
2016-02-29 23:28:12 -0800

    Added opcode error exception class, a driver, and modified the makefile to build both programs.

---
+++ opcode_error_exception.h
@@ -0,0 +1,30 @@
+/* Shad Aziz, Phillip Domann, Melanie Reed, Matt Walther
+   mascxxxx
+   Team Idaho
+   prog2
+   CS530, Spring 2016
+ */
+
+#ifndef opcode_error_exception_h
+#define opcode_error_exception_h
+
+using namespace std;
+
+class opcode_error_exception {
+public:
+    opcode_error_exception {
+        message = "An opcode error has occured.";
+    }
+    opcode_error_exception(string s) {
+        message = s;
+    }
+    
+    string getMessage() {
+        return message;
+    }
+private:
+    string message;
+};
+
+
+#endif
---
+++ sicxeoptab.cpp
@@ -0,0 +1,12 @@
+/* Shad Aziz, Phillip Domann, Melanie Reed, Matt Walther
+   mascxxxx
+   Team Idaho
+   prog2
+   CS530, Spring 2016
+ */
+
+using namespace std;
+
+int main(int argc, const char *argv[]) {
+    return 0;
+}
