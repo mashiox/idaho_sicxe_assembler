fa8f5e0948bcfb733fbfa69883a08300063fa36d
Shad Aziz <shadaziz95@yahoo.com>
2016-04-04 21:48:46 -0700

    Update symtab.h

--- symtab.h
+++ symtab.h
@@ -1,9 +1,9 @@
-/* Shad Aziz, Phillip Domann, Melanie Reed, Matt Walther
- mascxxxx
- Team Idaho
- prog3
- CS530, Spring 2016
- */
+/* Phillip Domann, Shad Aziz, Melanie Reed, Matt Walther
+   masc0832
+   Team Idaho
+   prog3
+   CS530, Spring 2016
+*/
 
 #ifndef SYMTAB_H
 #define SYMTAB_H
