49a8265a269ca07d7d84794dfb37874858e69b47
Phillip Domann <phillip.domann@gmail.com>
2016-04-05 06:06:53 -0700

    Cleaned up the handlers to remove duplicate code. Reads the proper values from the operands for directives. Added better error message handling. Add code to put the line addresses in a vector. Modified the symtab to hold strings so all values could be put in. Also to add all the label addresses in the symtab. Don't know entirely if the symbols are good but shouldn't matter for prog3. Compiles without errors or warnings on rohan with the makefile. Added the header to the .lis file. The listing_lnout could use better formatting. Works with the 4 source files and one I copied from the book.

--- sicxe_asm.cpp
+++ sicxe_asm.cpp
@@ -1,9 +1,9 @@
 /* Shad Aziz, Phillip Domann, Melanie Reed, Matt Walther
- mascxxxx
- Team Idaho
- prog3
- CS530, Spring 2016
- */
+   masc0832
+   Team Idaho
+   prog3
+   CS530, Spring 2016
+*/
 
 #include <iomanip>
 #include <iostream>
@@ -18,47 +18,67 @@
 
 using namespace std;
 
+int hextoi(string str) {
+    int integer;
+    stringstream ss;
+    ss << hex << str;
+    ss >> integer;
+    return integer;
+}
+
+bool isdecimal(string& str, size_t start, size_t end) {
+    for (size_t i = start; i < end; ++i) {
+        if (!isdigit(str[i])) {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool ishexadecimal(string& str, size_t start, size_t end) {
+    string upper = str;
+    transform(upper.begin(),upper.end(), upper.begin(), ::toupper);
+    for (size_t i = start; i < end; ++i) {
+        if (!(isdigit(upper[i]) || (65 <= upper[i] && upper[i] <= 70))) {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool isconstant(string& str) {
+    if (str.empty())
+        return false;
+    return (str[0] == '$') ? ishexadecimal(str, 1, str.length()) : isdecimal(str, 0, str.length());
+}
+
+int ctoi(string& str) {
+    return (str[0] == '$') ? hextoi(str.substr(1)): atoi(str.c_str());
+}
+
+bool isquoted(string& str) {
+    return ((str[1] == '\'') && (str[str.length()-1] == '\'')) ? true : false;
+}
+
+bool isliteral(string& str) {
+    if (str.empty() || str.length() < 3)
+        return false;
+    if ((str[0] == 'c') || (str[0] == 'C'))
+        return isquoted(str);
+    else if (((str[0] == 'x') || (str[0] == 'X')) && (str.length()%2 == 1)) {
+        return (isquoted(str)) ? ishexadecimal(str, 2, str.length()-1): false;
+    }
+    return false;
+}
+
+unsigned int size_for_literal(string& str) {
+    return ((str[0] == 'c') || (str[0] == 'C')) ? (unsigned int)str.length()-3: (unsigned int)(str.length()-3)>>1;
+}
+
 string itos(int integer) {
     stringstream itoss;
     itoss << integer;
     return itoss.str();
-}
-
-bool is_hex_string( string hex ){
-	if ( hex.length() == 0 ) return false;
-	
-	if ( hex.compare(0, 1, "x" ) == 0 ) return true;
-	return false;
-}
-
-bool is_char_string( string char_string ){
-	if ( char_string.length() == 0 ) return false;
-	
-	if ( char_string.compare(0, 1, "c" ) == 0 ) return true;
-	return false;
-}
-
-string get_byte_literal( string literal ){
-	// This is the wrong thing to do, we need to throw an exception instead.
-	if ( literal.length() == 0 ) return string("");
-	
-	return literal.substr( 2, literal.length()-3 );
-}
-
-int* string_to_int( string key ){
-	int* iptr = new int[ key.length() ];
-	for ( size_t idx = 0 ; idx < key.length() ; idx++ ){
-		( iptr +idx ) = (int)key[idx];
-    }
-    return iptr;
-}
-
-int* string_to_hex( string literal ){
-	int *iptr = new int[ 1 ];
-	stringstream ss;
-	ss << std::hex << literal;
-    ss >> *iptr;
-	return iptr;
 }
 
 bool is_start( string opcode ){
@@ -71,17 +91,45 @@ bool is_end( string opcode ){
     return ( opcode.compare("END") == 0 ? true : false );
 }
 
+const struct sicxe_asm::dhpair sicxe_asm::dhpairs[9] = {
+    {"START", &sicxe_asm::handle_start},
+    {"END", &sicxe_asm::handle_directive},
+    {"BYTE", &sicxe_asm::handle_byte_word},
+    {"WORD", &sicxe_asm::handle_byte_word},
+    {"RESB", &sicxe_asm::handle_resb},
+    {"RESW", &sicxe_asm::handle_resw},
+    {"BASE", &sicxe_asm::handle_directive},
+    {"NOBASE", &sicxe_asm::handle_directive},
+    {"EQU", &sicxe_asm::handle_equ}
+};
+
 sicxe_asm::sicxe_asm(string file) {
     parser = new file_parser(file);
+    locctr = 0;
     setup_handler_map();
-    string listingFile = file.substr(0, file.find(".",0));
+    string listingFile = file.substr(0, file.rfind("."));
     listingFile = listingFile + ".lis";
     listing.open(listingFile.c_str());
-    listing_head(listingFile);
+    listing_head(file);
 }
 
 sicxe_asm::~sicxe_asm() {
+    listing.close();
     delete parser;
+}
+
+void sicxe_asm::error_str(string msg) {
+    string str = "***Error***\n";
+    str.append(msg);
+    throw str;
+}
+
+void sicxe_asm::error_ln_str(string msg) {
+    string str = "***Error***\n";
+    string lnmsg = "Line "+itos(index+1)+": "+label+" "+opcode+" "+operand+"\n";
+    str.append(lnmsg);
+    str.append(msg);
+    throw str;
 }
 
 void sicxe_asm::pass1() {
@@ -92,22 +140,26 @@ void sicxe_asm::pass1() {
         get_tokens();
         if (is_start(opcode)) {
             handle_start();
+            listing_lnout();
             break;
         } else if (!opcode.empty()) {
-            throw string("Error: Line "+itos(index+1)+", There must be no operations before start directive.");
+            error_ln_str("There must be no operations before start directive.");
         } else {
-            handle_empty();
+            listing_lnout();
+            line_addrs.push_back(locctr);
         }
     }
     
     if (index == nlines) {
-        throw string("Error: There is no start directive in sourcefile.");
+        error_str("There is no start directive in sourcefile.");
     }
     sym_handler handle_symbol;
     for (++index; index < nlines; ++index) {
+        line_addrs.push_back(locctr);
         get_tokens();
+        listing_lnout();
         if (is_end(opcode)) {
-            handle_end();
+            handle_directive();
             break;
         }
         else {
@@ -117,14 +169,15 @@ void sicxe_asm::pass1() {
     }
     
     if (index == nlines) {
-        throw string("Error: There is no end directive in sourcefile.");
+        error_str("There is no end directive in sourcefile.");
     }
     for (++index; index < nlines; ++index) {
+        line_addrs.push_back(locctr);
         get_tokens();
+        listing_lnout();
         if (!opcode.empty()) {
-            throw string("Error: Line "+itos(index+1)+" Additional operations exist after end directive.");
+            error_ln_str("Additional operations cannot exist after end directive.");
         }
-        handle_empty();
     }
     
 }
@@ -137,21 +190,21 @@ void sicxe_asm::get_tokens() {
 }
 
 void sicxe_asm::listing_head(string filename) {
-	listing << "Line#" << setw(16) << "Address" << setw(16) <<"Label" << setw(16) << "Opcode" << setw(36) << "Operand\n";
-	listing << "=====" << setw(16) << "=======" << setw(16) <<"=====" << setw(16) << "======" << setw(36) << "=======\n";
+    int width = 38-(int)(filename.length()>>1);
+    listing << setw(width) << "**" << filename << "**" << "\n\n";
+    listing << setw(5) << "Line#" << setw(8) << " Address" << setw(16);
+    listing << " Label" << setw(16) << "Opcode" << setw(36) << "Operand\n";
+    listing << setw(5) << "=====" << setw(8) << " =======" << setw(16);
+    listing << " =====" << setw(16) << "======" << setw(36) << "=======\n";
 }
 
-void sicxe_asm::addto_listing() {
-    listing << dec << index+1 << setw(16) << hex << uppercase << setfill('0') << locctr << setw(16) << setfill(' ') << label << setw(16) << opcode << setw(36) << operand << "\n";
+void sicxe_asm::listing_lnout() {
+    listing << setw(5) << dec << index+1;
+    listing << "   " << setw(5) << hex << uppercase << setfill('0') << locctr;
+    listing << setw(16) << setfill(' ') << label;
+    listing << setw(16) << opcode;
+    listing << setw(36) << operand << "\n";
 }
-
-//void sicxe_asm::write_listing() {
-//    listing.str() << ;
-//}
-
-//void sicxe_asm::print_listing() {
-//    cout << listing.str();
-//}
 
 void sicxe_asm::setup_handler_map() {
     unsigned int i;
@@ -174,132 +227,92 @@ sicxe_asm::sym_handler sicxe_asm::handler_for_symbol() {
     transform(symbol.begin(), symbol.end(), symbol.begin(), ::toupper);
     map<string, sym_handler>::iterator iter = hmap.find(symbol);
     if (iter == hmap.end()) {
-        throw string("Error: Line "+itos(index+1)+", invalid opcode "+opcode+".");
+        error_ln_str("Invalid opcode.");
     }
     return iter->second;
 }
 
 void sicxe_asm::handle_instruction() {
-    int size = optab.get_instruction_size(opcode);
-    addto_listing();
-    locctr += size;
+    try {
+        locctr += optab.get_instruction_size(opcode);
+    }
+    catch (opcode_error_exception e) {
+        error_ln_str(e.getMessage());
+    }
+    add_symbol_for_label();
 }
+
 void sicxe_asm::handle_start() {
-    /**
-    * NB
-	* start_address comes off the file as a hex string
-	* use string_to_hex( string ) to convert to int*.
- 	*/
-	
-	/**
-	* TODO
-	* Save symtab[label] = start_address
-	*
-	* Set LOCCTR to start_address
-	*
-	* Aux function to write intermediate file.
-	*/
-
-	/**
-	* NEEDS
-	* Aux function for hex str -> int
-	*/
-    addto_listing();
+    if (isconstant(operand))
+        locctr = ctoi(operand);
+    else
+        error_ln_str("Invalid constant.");
+    
+    try {
+        symbols.add(opcode, label);
+    }
+    catch (symtab_exception e) {
+        error_ln_str(e.getMessage());
+    }
 }
 
-void sicxe_asm::handle_end() {
-    /**
-	* TODO
-	* Set program length to LOCCTR.
-	*/
-	addto_listing();
-}
-
-void sicxe_asm::handle_byte() {
-    string literal;
-	size_t byte_length;
-	int* int_code;
-	/**
-	* BYTE x'abcd' case
-	*/
-	if ( is_hex_string(operand) ){
-		literal = get_byte_literal( operand );
-		if ( literal.length() != 4 ) return; // TODO, THROW EXCEPTION
-		byte_length = 4;
-        int_code = string_to_hex( literal );
-    }
-	/**
-	* BYTE c'abcdefg' case
-	*/
-	else if ( is_char_string(operand) ){
-		literal = get_byte_literal( operand );
-		byte_length = literal.length();
-		int_code = string_to_int( literal );
-    }
-	else {
-	// THROW EXCEPTION
-	/**
-	* TODO
-	* Save symtab[label] = int_code
-	* increment locctr by byte_length. locctr += byte_length.
-	*/
-    }
-	addto_listing();
-}
-
-void sicxe_asm::handle_word() {
-    /**
-	* Save symtab[label] = constant
-	* Increment LOCCTR by 3, locctr += 3
-	*/
-	addto_listing();
+void sicxe_asm::handle_byte_word() {
+    if (isliteral(operand))
+        locctr += size_for_literal(operand);
+    else if (isconstant(operand))
+        locctr += (opcode[0] == 'b' || opcode[0] == 'B') ? 1: 3;
+    else
+        error_ln_str("Invalid quoted literal.");
+    add_symbol_for_label();
 }
 
 void sicxe_asm::handle_resb() {
-    /**
-	* Same issue as handle_resw()
-	*/
-    //convert operand to int and add to locctr
-	//int* reserved_space = new int[ operand ];
-	/**
-    * TODO
-	* save symtab[label] = reserved_space
-	* increment locctr by constant, locctr += constant
-	*/
-	addto_listing();
+    if (isconstant(operand))
+        locctr += ctoi(operand);
+    else
+        error_ln_str("Invalid constant.");
+    add_symbol_for_label();
 }
 
 void sicxe_asm::handle_resw() {
-    /**
-	WARN: Unsure if this is the right approach to be compatible with SYMTAB.
-	However, even if chars are stored in this space, they can be interpreted as ints
-	because of ASCII.
-	It might be good if SYMTAB was a map of int*s though... then everything is consistent.
-	*/
-    //convert operand to int and add to locctr
-	//int* reserved_space = new int[ 3*operand ];
-	/**
-	* Save symtab[label] = reserved_space;
-	*
-	* Increment locctr by 3*constant, locctr += 3*constant
-	*/
-	addto_listing();
-}
-
-void sicxe_asm::handle_base() {
-	addto_listing();
-}
-
-void sicxe_asm::handle_nobase() {
-	addto_listing();
+    if (isconstant(operand))
+        locctr += ctoi(operand)*3;
+    else
+        error_ln_str("Invalid constant.");
+    add_symbol_for_label();
 }
 
 void sicxe_asm::handle_equ() {
-	addto_listing();
+    try {
+        symbols.add(label, operand);
+    }
+    catch (symtab_exception e) {
+        error_ln_str(e.getMessage());
+    }
+}
+
+void sicxe_asm::handle_directive() {
+    try {
+        symbols.add(opcode, operand);
+    }
+    catch (symtab_exception e) {
+        error_ln_str(e.getMessage());
+    }
 }
 
 void sicxe_asm::handle_empty() {
-    addto_listing();
+    add_symbol_for_label();
+}
+
+void sicxe_asm::add_symbol_for_label() {
+    if (!label.empty()) {
+        try {
+            symbols.add(label, itos(locctr));
+        }
+        catch (symtab_exception e) {
+            error_ln_str(e.getMessage());
+        }
+    }
 }
 
 int main(int argc, char* argv[]) {
@@ -311,8 +324,6 @@ int main(int argc, char* argv[]) {
     
     try {
     	assembler.pass1();
-    	//assembler.write_listing();
-    	//assembler.print_listing();
         return 0;
     }
     catch (file_parse_exception fpe) {
--- sicxe_asm.h
+++ sicxe_asm.h
@@ -1,9 +1,9 @@
 /* Shad Aziz, Phillip Domann, Melanie Reed, Matt Walther
- mascxxxx
- Team Idaho
- prog3
- CS530, Spring 2016
- */
+   masc0832
+   Team Idaho
+   prog3
+   CS530, Spring 2016
+*/
 
 #ifndef SICXE_ASM_H
 #define SICXE_ASM_H
@@ -18,13 +18,13 @@ using namespace std;
 class sicxe_asm {
     typedef void (sicxe_asm::*sym_handler)();
     
-    file_parser* parser = NULL;
+    file_parser* parser;
     opcodetab optab;
     symtab symbols;
     map<string, sym_handler> hmap;
     vector<unsigned int> line_addrs;
     unsigned int index;
-    unsigned int locctr = 0;
+    unsigned int locctr;
     string label;
     string opcode;
     string operand;
@@ -36,42 +36,33 @@ class sicxe_asm {
         sym_handler handler;
     };
     
-    const struct dhpair dhpairs[9] = {
-        {"START", &sicxe_asm::handle_start},
-        {"END", &sicxe_asm::handle_end},
-        {"BYTE", &sicxe_asm::handle_byte},
-        {"WORD", &sicxe_asm::handle_word},
-        {"RESB", &sicxe_asm::handle_resb},
-        {"RESW", &sicxe_asm::handle_resw},
-        {"BASE", &sicxe_asm::handle_base},
-        {"NOBASE", &sicxe_asm::handle_nobase},
-        {"EQU", &sicxe_asm::handle_equ}
-    };
+    static const struct dhpair dhpairs[9];
     
     void get_tokens();
     void listing_head(string filename);
-    void addto_listing();
+    void listing_lnout();
     
     void setup_handler_map();
     sym_handler handler_for_symbol();
     void handle_instruction();
     void handle_start();
-    void handle_end();
-    void handle_byte();
-    void handle_word();
+    void handle_byte_word();
     void handle_resb();
     void handle_resw();
-    void handle_base();
-    void handle_nobase();
     void handle_equ();
+    void handle_directive();
     void handle_empty();
+    
+    void add_symbol_for_label();
+    
+    void error_str(string msg);
+    void error_ln_str(string msg);
     
 public:
     sicxe_asm(string file);
     ~sicxe_asm();
     
     void pass1();
-    void print_listing();
     
 };
 
